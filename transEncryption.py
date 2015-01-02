c76e626317fb977de54326dd6a5361723e086ce0
vdrey <daniel7reynolds@gmail.com>
2015-01-01 23:50:16 -0500

    Edit transEncryption

--- transEncryption.py
+++ transEncryption.py
@@ -1,1 +1,18 @@
 #Transposition Cipher Encryption
+
+import pyperclip
+
+def main():
+    myMessage = input('Message: ')
+    myKey = input('Key: ')
+
+    ciphertext = encryptMessage(myKey, myMessage)
+
+    #Add a | to the end of the ciphertext
+
+    print(ciphertext + '|')
+
+    #Copy the string to the keyboard
+    pyperclip.copy(ciphertext)
+
+    
