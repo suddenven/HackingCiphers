8f09f34ced08ad9a2d6961a9e89934088f231d02
vdrey <daniel7reynolds@gmail.com>
2015-01-01 23:59:31 -0500

    Edit transEncryption

--- transEncryption.py
+++ transEncryption.py
@@ -4,7 +4,7 @@ import pyperclip
 
 def main():
     myMessage = input('Message: ')
-    myKey = input('Key: ')
+    myKey = int(input('Key: '))
 
     ciphertext = encryptMessage(myKey, myMessage)
 
@@ -16,3 +16,28 @@ def main():
     pyperclip.copy(ciphertext)
 
     
+def encryptMessage(key, message):
+
+    #Each string in ciphertext represents a column in the grid.
+    ciphertext = [''] * key
+
+    #Loop through each column in ciphertext.
+    for col in range(key):
+        pointer = col
+
+        #Keep looping until pointer goes past the length of the message.
+        while pointer < len(message):
+            #Place the character at pointer in message at the end of the
+            #Current column in the ciphertext list.
+            ciphertext[col] += message[pointer]
+
+            #move pointer over
+            pointer += key
+
+    #Convert the ciphertext list into a single string value and return it.
+    return ''.join(ciphertext)
+
+#If tranpositionEncrypt.py is run (instead of imported as a module) call
+#the main() function
+if __name__ == '__main__':
+    main()
