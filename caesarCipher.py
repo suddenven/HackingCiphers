1f9bc5dade1dbd500d7d84e57f5ab55c22709f83
Daniel Reynolds <vdrey@users.noreply.github.com>
2015-01-01 20:02:53 -0500

    Edit Caesar

--- caesarCipher.py
+++ caesarCipher.py
@@ -26,4 +26,24 @@ for symbol in message:
     if symbol in LETTERS:
         #Get the number for this symbol
         num = LETTERS.find(symbol)
-        if mode == 'encrypt'
+        if mode == 'encrypt':
+            num = num + key
+        elif mode == 'decrypt':
+            num = num - key
+        #handle wraparound
+        if num >= len(LETTERS):
+            num = num - len(LETTERS)
+        elif num < 0:
+            num = num + len(LETTERS)
+
+        #add the symbol
+        translated = translated + LETTERS[num]
+
+    else:
+        translated = translated + symbol
+
+# print translation to the screen
+print(translated)
+
+#Copy the translated string to the clipboard
+pyperclip.copy(translated)
