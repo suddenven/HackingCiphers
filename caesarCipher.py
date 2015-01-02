69c16349024c4c3f6f2fb52804278abe6b3a7417
Daniel Reynolds <vdrey@users.noreply.github.com>
2015-01-01 19:49:18 -0500

    Edits

--- caesarCipher.py
+++ caesarCipher.py
@@ -1,1 +1,15 @@
 #Caesar Cipher
+
+import pyperclip
+
+#The string to be encrypted
+message = input('Enter message:')
+
+#The encryption key
+key = input('Enter an integer between 0 and 25, inclusive')
+
+#Encrypt or Decrypt?
+mode = input('encrypt or decrypt:')
+
+#Every possible symbol
+LETTERS =
