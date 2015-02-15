4ceca9a6b22b1d585c0439a2ba84e1c2d9078cfc
Daniel Reynolds <vdrey@users.noreply.github.com>
2015-02-15 16:56:40 -0500

    Formatting

--- Transposition/detectEnglish.py
+++ Transposition/detectEnglish.py
@@ -7,6 +7,7 @@
 # There must be a dictionary.txt file in this directory with all English
 # words in it, one word per line. You can download this from
 # http://invpy.com/dictionary.txt
+
 UPPERLETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 LETTERS_AND_SPACE = UPPERLETTERS + UPPERLETTERS.lower() + ' \t\n'
 
