0b3c1e4c072edc6c2cac960b4c98c3333fd56dec
Daniel Reynolds <vdrey@users.noreply.github.com>
2015-02-16 20:03:33 -0500

    Edit affineCipher

--- Affine/affineCipher.py
+++ Affine/affineCipher.py
@@ -3,3 +3,22 @@
 
 import sys, pyperclip, cryptomath, random
 SYMBOLS = """ !'#$%&'()*=,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_'abcdefghijklmnopqrstuvwxyz{|}~""" # Note the space at the front
+
+
+def main():
+    myMessage = """"A computer would deserve to be called intelligent if it could deceive a human into believing that it was human." -Alan Turing"""
+    myKey = 2023
+    myMode = 'encrypt' # set to 'encrypt' or 'decrypt'
+
+    if myMode == 'encrypt':
+        translated = encryptMessage(myKey, myMessage)
+    elif myMode == 'decrypt':
+        translated = decryptMessage(myKey, myMessage)
+    print('Key: %s' % (myKey))
+    print('%sed text:'% (myMode.title()))
+    print(translated)
+    pyperclip.copy(translated)
+    print('Full %sed text copied to clipboard.' % (myMode))
+
+
+def getKeyParts(key):
