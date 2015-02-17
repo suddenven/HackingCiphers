f21d2b6cb6a58c089ba1a305482170a338857072
Daniel Reynolds <vdrey@users.noreply.github.com>
2015-02-16 19:53:38 -0500

    Started affineCipher

---
+++ Affine/affineCipher.py
@@ -0,0 +1,5 @@
+# Affine Cipher
+#
+
+import sys, pyperclip, cryptomath, random
+SYMBOLS = """ !'#$%&'()*=,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_'abcdefghijklmnopqrstuvwxyz{|}~""" # Note the space at the front
